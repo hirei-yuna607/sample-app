3c850691fe8900b62c1eb0c75173eb0137811460
hirei-yuna607 <77141336+hirei-yuna607@users.noreply.github.com>
2021-07-12 13:39:26 +0900

    global-style-chakra-ui-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Button colorScheme="teal">ボタン</Button>
+        <p>aaaa</p>
       </ChakraProvider>
     </div>
   );
